3b6a1a2ffa186185b7711cd3fba19d51ffc21166
Eric Mong <m.changhsi@gmail.com>
2024-09-26 00:15:01 +0800

    Add spinner loader

--- src/components/Shared/Root/index.tsx
+++ src/components/Shared/Root/index.tsx
@@ -5,8 +5,17 @@ import { ErrorBoundary } from "../Error/ErrorBoundary";
 import ErrorView from "@/views/ErrorView";
 import { PropsWithChildren } from "react";
 import RootProviders from "./RootProviders";
+import Spinner from "../Loader/Spinner";
+import { styled } from "styled-components";
 
-// TODO: Update loading UI
+const LoadingPage = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 const Root = (props: PropsWithChildren) => {
   const didMount = useDidMount();
 
@@ -15,7 +24,9 @@ const Root = (props: PropsWithChildren) => {
       <RootProviders {...props} />
     </ErrorBoundary>
   ) : (
-    <div className="root__loading">Loading</div>
+    <LoadingPage>
+      <Spinner />
+    </LoadingPage>
   );
 };
 
