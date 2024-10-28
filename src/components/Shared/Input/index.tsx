f08f53b5db113826fa0b23fbefa690ae833130c4
Eric Mong <m.changhsi@gmail.com>
2024-10-28 23:34:24 +0800

    Update input component

--- src/components/Shared/Input/index.tsx
+++ src/components/Shared/Input/index.tsx
@@ -1,15 +1,84 @@
-import { FC } from "react";
-import { InputContainer } from "./style";
+import { FC, useEffect, useState } from "react";
+import {
+  InputClearButton,
+  InputContainer,
+  InputContent,
+  InputIcon,
+} from "./style";
+import Image from "next/image";
+import RemoveSVG from "@/assets/icons/remove.svg";
 
 interface IProps {
-  value: string;
+  value?: string;
   type: string;
-  onChange: (event: any) => void;
+  onChange: (value: string) => void;
   placeholder: string;
+  size?: "tiny" | "small" | "medium" | "large";
+  disabled?: boolean;
+  startIcon?: JSX.Element | string;
+  hasClear?: boolean;
 }
 
-const Input: FC<IProps> = (props) => {
-  return <InputContainer {...props} />;
+const RenderIcon = (
+  icon: JSX.Element | string,
+  alt: string,
+  size: "tiny" | "small" | "medium" | "large"
+) => {
+  if (typeof icon === "string") {
+    return (
+      <InputIcon size={size}>
+        <Image src={icon} alt={alt} fill />
+      </InputIcon>
+    );
+  }
+  return <>{icon}</>;
+};
+
+const Input: FC<IProps> = ({
+  value = "",
+  type = "text",
+  onChange = () => {},
+  placeholder = "",
+  size = "small",
+  disabled = false,
+  startIcon,
+  hasClear = true,
+}) => {
+  const [inputValue, setInputValue] = useState<string>(value);
+
+  useEffect(() => {
+    setInputValue(value);
+  }, [value]);
+
+  const handleClear = () => {
+    setInputValue("");
+    onChange("");
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setInputValue(newValue);
+    onChange(newValue);
+  };
+
+  return (
+    <InputContainer size={size}>
+      {startIcon && RenderIcon(startIcon, placeholder ?? "", size)}
+      <InputContent
+        size={size}
+        placeholder={placeholder}
+        value={inputValue}
+        type={type}
+        onChange={handleChange}
+        disabled={disabled}
+      />
+      {hasClear && (
+        <InputClearButton onClick={handleClear} size={size}>
+          <Image src={RemoveSVG} alt="remove" fill />
+        </InputClearButton>
+      )}
+    </InputContainer>
+  );
 };
 
 export default Input;
--- src/components/Shared/Input/style.tsx
+++ src/components/Shared/Input/style.tsx
@@ -1,5 +1,86 @@
-import { styled } from "styled-components";
+import { css, styled } from "styled-components";
 
-export const InputContainer = styled.input`
-  padding: 12px 18px;
+export const InputContainer = styled.div<{
+  size: "tiny" | "small" | "medium" | "large";
+}>`
+  position: relative;
+  width: 100%;
+  box-sizing: border-box;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  border-width: 2px;
+  border-style: solid;
+
+  ${({ theme, size }) => css`
+    border-radius: ${theme.spacing.medium};
+    padding: ${theme.spacing.medium} ${theme.spacing.large};
+  `}
+`;
+
+export const InputIcon = styled.div<{
+  size: "tiny" | "small" | "medium" | "large";
+}>`
+  position: relative;
+  aspect-ratio: 1/1;
+
+  ${({ theme, size }) => css`
+    height: ${size === "tiny"
+      ? theme.fontSizes.medium
+      : size === "small"
+        ? theme.fontSizes.large
+        : size === "medium"
+          ? theme.fontSizes.large
+          : theme.fontSizes.xlarge};
+  `}
+`;
+
+export const InputContent = styled.input<{
+  size: "tiny" | "small" | "medium" | "large";
+}>`
+  width: 100%;
+
+  border: none;
+  outline: none;
+  box-shadow: none;
+
+  ${({ theme, size }) => css`
+    font-size: ${size === "tiny"
+      ? theme.fontSizes.small
+      : size === "small"
+        ? theme.fontSizes.medium
+        : size === "medium"
+          ? theme.fontSizes.large
+          : theme.fontSizes.xlarge};
+
+    border-radius: ${theme.spacing.medium};
+  `}
+
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+`;
+
+export const InputClearButton = styled.button<{
+  size: "tiny" | "small" | "medium" | "large";
+}>`
+  position: relative;
+  border: none;
+  margin: 0;
+  padding: 0;
+  aspect-ratio: 1/1;
+  background: transparent;
+
+  ${({ theme, size }) => css`
+    height: ${size === "tiny"
+      ? theme.fontSizes.small
+      : size === "small"
+        ? theme.fontSizes.medium
+        : size === "medium"
+          ? theme.fontSizes.medium
+          : theme.fontSizes.medium};
+  `}
 `;
